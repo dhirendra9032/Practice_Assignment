aea9a80c6e2181979b8c38064900fd5ab54c2d6e
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-08-15 11:00:29 +0530

    Subsquence recursion program

---
+++ DSA_Assignment/Recursion/subsequence.js
@@ -0,0 +1,49 @@
+//Sub Sequence using recursion
+
+//Normal way
+/*let str="abc";
+let res="";
+
+function generateSubSequence(str,  res)
+{
+      if(str.length==0)
+            {
+           console.log(res);
+            return
+      }
+      generateSubSequence(str.substring(1),res)//character is not present[0]
+      generateSubSequence(str.substring(1),res+str.charAt(0))//character is present[1]
+}
+function main(str,res)
+{generateSubSequence(str,res);//main function we are calling orginal string and empty string
+}
+
+main(str,res);
+*/
+
+// Second ways according lexicographicaly
+
+let str=["a","b","c","d"];
+let res="";
+
+function subsequence(res,str,low, high)
+    {
+        if(low<=high+1 && res!="") 
+        {
+            console.log(res);
+        }
+        for(var i=low;i<=high;i++)
+        {
+            subsequence(res+str[i],str,i+1,high);
+        }
+    }
+    
+  function  main(str,res)
+    {
+    
+       subsequence(res,str,0,str.length-1);
+    }
+
+    main(str,res);
+
+    
