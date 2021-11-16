14378753ce54de38665f31359dd9d4862f37a5a9
Dhirendra Kumar <dhirendra9032@gmail.com>
2021-11-16 12:47:55 +0530

    Image Op

---
+++ Next Js/todo_next_js/pages/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className="container d-flex justify-content-center bg-warning">
+        <button className="btn btn-primary  ">Add New User</button>
+    </div>
+  )
+}
