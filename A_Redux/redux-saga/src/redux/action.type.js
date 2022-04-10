c2e691a27a4f1a8da00135a4f1432f12166fcc78
Dhirendra Kumar <dhirendra9032@gmail.com>
2022-04-10 18:37:27 +0530

    chang

--- A_Redux/redux-saga/src/redux/action.type.js
+++ A_Redux/redux-saga/src/redux/action.type.js
@@ -2,5 +2,6 @@ export const Add = 'Add';
 export const Subtract = 'Subtract';
 export const Multiply = 'Multiply';
 export const Increment = 'Increment';
-export const Decremet = 'Decremental';
+export const Decremet = 'Decrement';
 //tu mera hai sanam 
+//is is changes that tyou want
